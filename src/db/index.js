5c0f17e84b303b3a312fdb6e9bc49d52d3df711c
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-05-12 00:40:47 +0530

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -1,16 +1,38 @@
+// import mysql from "mysql2/promise";
+
+// const connectDB = async () => {
+//   const connection = await mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+//   });
+
+//   console.log("✅ MySQL database connected successfully!");
+
+//   return connection;
+// };
+
+// export default connectDB;
+
 import mysql from "mysql2/promise";
 
 const connectDB = async () => {
-  const connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-  });
+  try {
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    });
 
-  console.log("✅ MySQL database connected successfully!");
-
-  return connection;
+    console.log("✅ MySQL database connected successfully!");
+    return connection;
+  } catch (error) {
+    console.error("❌ MySQL DB connection failed !!!", error);
+    throw error;
+  }
 };
 
 export default connectDB;
